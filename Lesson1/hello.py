64185b26dc6b08ac4a2fa365e53c516007ecb577
bhargavi0541 <bhargu5apr2011@gmail.com>
2026-06-12 23:38:25 +0530

    lesson 1 completed

---
+++ Lesson1/hello.py
@@ -0,0 +1,8 @@
+print("Hello!")
+print("4")
+print(4)
+print("4+5")
+print(4+5)
+name= "Bhargavi"
+print("The entered name is:" ,name)
+print("\n The entered name is:{name}")
---
+++ Lesson1/variable.py
@@ -0,0 +1,9 @@
+a=10
+b=10.15
+c="String"
+d= True
+print(a,b,c,d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
