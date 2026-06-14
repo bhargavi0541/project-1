3d2e9e7a821f65de5d0a454365b5968c06e19771
bhargavi0541 <bhargu5apr2011@gmail.com>
2026-06-14 22:53:21 +0530

    Lesson 3 completed

---
+++ Lesson3/notes.py
@@ -0,0 +1,19 @@
+# 1) Take the total withdrawal amount as input from the user and store it in `Amount`.
+# 2) Find how many 100-rupee notes are needed:
+# Divide `Amount` by 100 (whole number division) and store it in `note_1`.
+# 3) Find the remaining amount after taking out 100-rupee notes:
+# Use the remainder of `Amount` after dividing by 100.
+# 4) From the remaining amount, find how many 50-rupee notes are needed:
+# Divide the remainder by 50 (whole number division) and store it in `note_2`.
+# 5) Find the remaining amount after taking out 50-rupee notes:
+# Use the remainder after dividing by 50.
+# 6) From the remaining amount, find how many 10-rupee notes are needed:
+# Divide the remainder by 10 (whole number division) and store it in `note_3`.
+# 7) Print the number of 100-rupee notes, 50-rupee notes, and 10-rupee notes.
+amount= int(input("Enter the required amount"))
+note_1= amount//100
+remainder= amount%100
+note_2= remainder//50
+remainder= remainder%50
+note_3= remainder//10
+print(f"The amount of 100 rupee notes is {note_1} and of 50 rupee note is {note_2} and for 10 rupee is {note_3}")
