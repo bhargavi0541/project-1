61dc63fa1659a06fcf4a9c48d16e8db81c6ea8f6
bhargavi0541 <bhargu5apr2011@gmail.com>
2026-06-26 00:02:13 +0530

    Lesson10 completed

---
+++ Lessons/Lesson10/sum.py
@@ -0,0 +1,14 @@
+#sum of whole numbers
+i=int(input("Enter a number"))
+sum=0
+a=1
+#for a in range(1,i+1):
+#    sum=sum+a
+#print("The sum is" ,sum)
+while a<=i:
+    sum= sum+a
+    a+=1
+print("The sum is:" ,sum)    
+    
+
+
