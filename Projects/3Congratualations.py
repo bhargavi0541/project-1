60b638e6cea03a4bffbdbd2182e01c46af7a47ee
bhargavi0541 <bhargu5apr2011@gmail.com>
2026-06-26 22:50:56 +0530

    Redo Completed

--- Projects/3Congratualations.py
+++ Projects/3Congratualations.py
@@ -1,2 +1,16 @@
 Message=input("Enter a message for congratulating your friend")
 print(Message.upper())
+name= "Tanara Grover"
+city= "Toronto"
+description= "She does public speaking"
+print("The character at 0th index in name" ,name[0])
+print("The character at 2nd index in city" ,city[2])
+print("The character at last index in description" ,description[-1])
+print("The surname is " ,name[7:13])
+print("The length of the description is " ,len(name))
+first= input("Enter the first name: ")
+last= input("Enter the last name: ")
+print(first + last)
+print("The sun rises in the east"*2)
+print("The city in capital " ,city.upper())
+print("The city in lower " ,city.lower())
